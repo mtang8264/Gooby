741e6980dcce906070a26ec66974fa4a2dbe01b7
Michael Tang <mtang8264@gmail.com>
2020-03-24 14:46:29 -0400

    Added a simple arm to show the connection from hand to body

---
+++ Assets/Scripts/ArmPositioner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmPositioner : MonoBehaviour
+{
+    [SerializeField]
+    Transform body, hand;
+
+    void LateUpdate()
+    {
+        float distance = Vector3.Distance(body.position, hand.position);
+        Vector3 midpoint = Vector3.Lerp(body.position, hand.position, 0.5f);
+
+        float rise = hand.position.y - body.position.y;
+        float run = hand.position.x - body.position.x;
+        float angle = Mathf.Atan(rise / run) * Mathf.Rad2Deg;
+        Vector3 rot = new Vector3(0, 0, angle +90);
+
+        transform.localScale = new Vector3(0.07f, distance, 1f);
+        transform.position = midpoint;
+        transform.eulerAngles = rot;
+    }
+}
