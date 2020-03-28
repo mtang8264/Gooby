e6839dcf9ad770c3d3bfdb04f0e0572a9c722731
Michael Tang <mtang8264@gmail.com>
2020-03-28 19:25:10 -0400

    Created a simple death script and death particle

---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    [SerializeField]
+    float timeTillDestruction;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timeTillDestruction -= Time.deltaTime;
+        if(timeTillDestruction <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField]
+    Vector3 respawnLocation;
+    [SerializeField]
+    float deathY;
+
+    [SerializeField]
+    float respawnTime;
+
+    [SerializeField]
+    GameObject deathParticle;
+
+    Coroutine respawnRoutine;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        if(transform.position.y < deathY)
+        {
+            if (respawnRoutine == null)
+            {
+                GameObject temp = Instantiate(deathParticle);
+                temp.transform.position = transform.position;
+
+                temp.transform.LookAt(Vector3.zero);
+
+                respawnRoutine = StartCoroutine(Respawn());
+            }
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        rb.velocity = Vector2.zero;
+        transform.localPosition = respawnLocation;
+        respawnRoutine = null;
+    }
+}
